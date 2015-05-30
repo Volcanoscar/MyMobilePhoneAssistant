074247ca6cee2d687beada2467e9bbf5d922cc14
cbooy <602840765@qq.com>
2015-05-30 17:48:27 +0800

    初始化项目

---
+++ src/com/cbooy/mmpa/BootActivity.java
@@ -0,0 +1,13 @@
+package com.cbooy.mmpa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BootActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_boot);
+	}
+}
